c93a6e4ed0ab076626da4eb18b85af60f7ce8510
Ugur Yüksel <uyueksel@sw.buhl-data.com>
2020-12-13 20:10:31 +0100

    initialize

---
+++ azureWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace azureWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
